ae5001d251582f3e9875815385207f7fef021217
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-07-26 20:53:31 +0800

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
+import router from './router'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.use(router)
 
 app.mount('#app')
