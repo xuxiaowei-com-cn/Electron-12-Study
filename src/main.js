11eb52f3988e2a62cab39c6370632d6f0a214eb6
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-07-26 21:01:44 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
+app.use(store)
 
 app.mount('#app')
